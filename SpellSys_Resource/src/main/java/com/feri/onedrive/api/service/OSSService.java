82328e68e7aed28b49217c3833f800851a3ea9d3
user <2715115281@qq.com>
2019-09-19 21:51:33 +0800

    Initial commit

---
+++ SpellSys_Resource/src/main/java/com/feri/onedrive/api/service/OSSService.java
@@ -0,0 +1,12 @@
+package com.feri.onedrive.api.service;
+
+
+import com.qfedu.common.vo.R;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface OSSService {
+    R saveFile(MultipartFile file) throws IOException;
+    R saveFiles(MultipartFile[] file) throws IOException;
+}
---
+++ src/main/java/com/qfedu/spellsys/SpellsysApplication.java
@@ -0,0 +1,13 @@
+package com.qfedu.spellsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpellsysApplication.class, args);
+    }
+
+}
