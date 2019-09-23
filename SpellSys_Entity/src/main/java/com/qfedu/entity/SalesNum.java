e46fac8cec30fd39af381452d59e77864034acbd
hzw <hzw_1995@aliyun.com>
2019-09-23 21:15:34 +0800

    实体类更新

---
+++ SpellSys_Entity/src/main/java/com/qfedu/entity/SalesNum.java
@@ -0,0 +1,14 @@
+package com.qfedu.entity;
+
+import lombok.Data;
+
+@Data
+public class SalesNum {
+    private Integer id;
+    //商品id
+    private Integer gId;
+    //销量
+    private Integer salesNum;
+
+
+}
